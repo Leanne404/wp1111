a73da65a6524f2243a7b32e5fc25ab110584d8bf
leanne404 <90459334+leanne404@users.noreply.github.com>
2022-10-06 02:55:06 +0800

    make title

---
+++ hw3/src/containers/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Title from './Title'
+
+function App() {
+  return (
+    <div className="todo-app__root">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
---
+++ hw3/src/containers/Title.js
@@ -0,0 +1,9 @@
+const  Title = () => {
+    return (  
+        <header className='todo-app__header'>
+            <p className="todo-app__title ">todos</p>
+        </header>
+    );
+}
+ 
+export default Title;
