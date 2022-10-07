438cbfdded309bea1cd639a2a35e1036f9435434
leanne404 <90459334+leanne404@users.noreply.github.com>
2022-10-07 23:02:33 +0800

    fix click each

--- hw3/src/components/Item.js
+++ hw3/src/components/Item.js
@@ -4,25 +4,26 @@ import x_png from '../img/x.png';
 const Item = ({input_todo, id, check_box}) => {
 
     function click_list(){
-        var todos = document.getElementsByClassName("todo-app__item-detail")
-        console.log("in",check_box)
+        var todos_checkbox = document.getElementById(id)
+        var todos = todos_checkbox.parentNode.parentNode.getElementsByClassName("todo-app__item-detail")
+        //console.log(todos[0])
         if(check_box === false){
-            console.log("f to t");
+            //console.log("f to t");
             check_box = true
             todos[0].style.textDecoration = "line-through";
             todos[0].style.opacity = 0.5;
-            console.log("t",check_box)
+            //console.log("t",check_box)
         }
         else{
-            console.log("t to f")
+            //console.log("t to f")
             check_box = false
             todos[0].style.textDecoration = "none";
             todos[0].style.opacity = 1;   
-            console.log("f",check_box)
+            //console.log("f",check_box)
         }
-        console.log("in2",check_box)
+        //console.log("in2",check_box)
     }
-    console.log("out",check_box)
+    //console.log("out",check_box)
 
     
     return ( 
