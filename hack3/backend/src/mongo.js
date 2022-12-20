828bf0536342e24a71c0e620588bb828d0a1f2fe
leanne404 <90459334+leanne404@users.noreply.github.com>
2022-12-20 09:26:05 +0800

    hack3

--- hack3/backend/src/mongo.js
+++ hack3/backend/src/mongo.js
@@ -7,7 +7,18 @@ mongoose.set("strictQuery", true);
 
 async function connect() {
   // TODO 1 Connect to your MongoDB and call dataInit()
+  mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
+  const mongo = mongoose.connection
+  const port = process.env.port || 5000
+
+  mongo.once('open', () => {
+    console.log('mongodb connected')
+    dataInit()
+  })
   // TODO 1 End
 }
 
