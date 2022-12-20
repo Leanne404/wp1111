2ca2570a83f4fda6480cce653f5b872b5247d386
leanne404 <90459334+leanne404@users.noreply.github.com>
2022-12-20 11:23:13 +0800

    hack3

--- hack3/frontend/src/components/Row.js
+++ hack3/frontend/src/components/Row.js
@@ -43,7 +43,12 @@ function Row({
     updateItem({
       variables: {
         // TODO 4 Use `updateItem` and pass the correct variables
-
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        date: item.date,
+        amount: item.amount,
+        category: item.category,
         // TODO End
         onError: (err) => {
           // eslint-disable-next-line no-console
