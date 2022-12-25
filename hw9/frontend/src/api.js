1a65cc1f4f4f56f9d4933637e04a87f349fc618e
leanne404 <90459334+leanne404@users.noreply.github.com>
2022-12-26 03:15:56 +0800

    change url

--- hw9/frontend/src/api.js
+++ hw9/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `http://localhost:4000/`,
+  baseURL: `http://wp1111-mpzx-9mneh7f6h-leanne404.vercel.app/`,
 });
 
 export default instance;
