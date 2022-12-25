a159149a27c77c36f0ca0f6b781c7c72e9d90f3e
leanne404 <90459334+leanne404@users.noreply.github.com>
2022-12-26 02:53:53 +0800

    url

--- hw9/frontend/src/api.js
+++ hw9/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `http://localhost:4000/`,
+  baseURL: `https://wp1111-2b3lbsfhc-leanne404.vercel.app`,
 });
 
 export default instance;
